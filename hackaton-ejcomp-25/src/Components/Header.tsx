0f08783a23a2b96ddb8f2d63c6e34f29e8755c71
Murilo M. <123572431+playerking3@users.noreply.github.com>
2025-10-20 21:03:27 -0300

    fonte de titulo adicionada

--- hackaton-ejcomp-25/src/Components/Header.tsx
+++ hackaton-ejcomp-25/src/Components/Header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     return (
         <header className="bg-principal flex justify-between items-center px-10">
             <Image src="/logo.png" alt="Logo Pet Lovers" width={100} height={100} />
-            <nav className="flex gap-10 text-white">
+            <nav className="flex gap-10 text-white font-bold font-title">
                 <Link href="/">Home</Link>
                 <Link href="/Loja">Loja</Link>
                 <Link href="/Consulta">Consulta</Link>
